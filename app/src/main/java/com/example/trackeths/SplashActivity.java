bdcdb5c1b2966ddc4ef2bcb8ae5b328d949de753
zihangg <kevu.lee@gmail.com>
2020-06-17 22:43:26 +0800

    added splash screen and animations

---
+++ app/src/main/java/com/example/trackeths/SplashActivity.java
@@ -0,0 +1,41 @@
+package com.example.trackeths;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    Animation topAnim, bottomAnim;
+    TextView tracketh, flavour;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_anim);
+        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_anim);
+
+        tracketh = findViewById(R.id.textView);
+        flavour = findViewById(R.id.descriptionView);
+
+        tracketh.setAnimation(topAnim);
+        flavour.setAnimation(bottomAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 5000);
+    }
+}
